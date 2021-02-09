65f713be9d075f644fef8c2c1dff3e362a6a64b6
Daniel <danielkarpen97@gmail.com>
2021-02-09 15:43:44 -0600

    Render table rows

--- components/Table/Body/index.js
+++ components/Table/Body/index.js
@@ -1,5 +1,20 @@
+import { catalog } from "~/lib";
+
+function renderTableRows() {
+  return catalog
+    .map(
+      (product) => `<tr>
+  <td>${product.category}</td>
+  <td>${product.name}</td>
+  <td>${product.price}</td>
+</tr>
+`
+    )
+    .join("\n");
+}
+
 export default () => `
 <tbody>
-// TODO Map over data from 'lib' and create these rows for each product
+  ${renderTableRows()}
 </tbody>
 `;
